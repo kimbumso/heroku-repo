4113be18d01691d8f97e5975bff9db19ef5f7744
kimbumso <k101413@naver.com>
2020-12-03 10:54:45 +0900

    add Quiz app

---
+++ quiz/views.py
@@ -0,0 +1,17 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Quiz
+from .serializers import QuizSerializer
+import random
+
+# Create your views here.
+@api_view(['GET'])
+def helloAPI(request):
+    return Response("hello world!")
+
+@api_view(['GET'])
+def randomQuiz(request, id):
+    totalQuizs = Quiz.objects.all()
+    randomQuizs = random.sample(list(totalQuizs), id)
+    serializer = QuizSerializer(randomQuizs, many=True)  # many=true 다수의 데이터에 대해서도 직렬화가 진행
+    return Response(serializer.data)
--- server_dev/settings.py
+++ server_dev/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'js4ha*cg35%@c83v^m7lfy8k586wp#
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = bool( os.environ.get('DJANGO_DEbuG', True))
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = ['*']
 
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
+    'ckeditor_uploader',
+    'rest_framework',
+    'quiz',
 ]
 
 MIDDLEWARE = [
@@ -119,9 +123,35 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor"
+
+# ck uploader
+CKEDITOR_UPLOAD_PATH = 'uploads/'  # create directory in media directory
+CKEDITOR_RESTRICT_BY_USER = True
+
+CKEDITOR_CONFIGS = {
+    "default": {
+        "removePlugins": "stylesheetparser",
+    }
+}
